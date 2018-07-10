5fb9d81e5fb678c1bd6b5ea7104f616d636f70e1
brunobelloni <bruno.belloni@hotmail.com>
2018-07-10 15:02:08 -0300

    commit

--- classifier/urls.py
+++ classifier/urls.py
@@ -6,11 +6,14 @@ urlpatterns = [
     path('images/', views.images, name='images'),
     path('images/new/', views.image_new, name='image_new'),
     path('images/<int:pk>/', views.image_detail, name='image_detail'),
+    path('images/edit/<int:pk>/', views.image_edit, name='image_edit'),
     path('images/delete/<int:pk>/', views.image_delete, name='image_delete'),
+
     path('insects/', views.insects, name='insects'),
     path('insects/new/', views.insect_new, name='insect_new'),
     path('insects/<int:pk>/', views.insect_detail, name='insect_detail'),
     path('insects/delete/<int:pk>/', views.insect_delete, name='insect_delete'),
+
     path('traps/', views.traps, name='traps'),
     path('traps/new/', views.trap_new, name='trap_new'),
     path('traps/<int:pk>/', views.trap_detail, name='trap_detail'),
--- classifier/views.py
+++ classifier/views.py
@@ -1,8 +1,6 @@
-from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from .models import Trap_Image, Trap, Insect
 from .forms import ImageForm, InsectForm, TrapForm
-from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -12,7 +10,6 @@ def index(request):
 @login_required
 def insects(request):
     insec = Insect.objects.all()
-
     return render(request, 'classifier/insect/main.html', {'insec': insec})
 
 @login_required
@@ -20,29 +17,28 @@ def insect_new(request):
     if request.method == "POST":
         form = InsectForm(request.POST)
         if form.is_valid():
-            insects = form.save()
+            form.save()
             form = InsectForm()
-            return HttpResponseRedirect(reverse('insects'))
+            return redirect('insects')
     else:
         form = InsectForm()
     return render(request, 'classifier/insect/new.html', {'form': form})
 
 @login_required
 def insect_delete(request, pk):
-    insect = get_object_or_404(Insect, pk=pk)
+    insect = Insect.objects.get(id=pk)
     insect.delete()
     return redirect('insects')
 
 @login_required
 def insect_detail(request, pk):
-    insect = get_object_or_404(Insect, pk=pk)
+    insect = Insect.objects.get(id=pk)
 
     return render(request, 'classifier/insect/detail.html', {'insect': insect})
 
 @login_required
 def images(request):
     display_img = Trap_Image.objects.all()
-
     return render(request, 'classifier/image/main.html', {'display_img': display_img})
 
 @login_required
@@ -52,8 +48,8 @@ def image_new(request):
     if request.method == "POST":
         form = ImageForm(request.POST)
         if form.is_valid():
-            image = form.save()
-            return HttpResponseRedirect(reverse('images'))
+            form.save()
+            return redirect('images')
     else:
         form = ImageForm()
 
@@ -61,19 +57,34 @@ def image_new(request):
 
 @login_required
 def image_delete(request, pk):
-    img = get_object_or_404(Trap_Image, pk=pk)
+    img = Trap_Image.objects.get(id=pk)
     img.delete()
     return redirect('images')
 
 def image_detail(request, pk):
-    img = get_object_or_404(Trap_Image, pk=pk)
-
+    img = Trap_Image.objects.get(id=pk)
     return render(request, 'classifier/image/detail.html', {'img': img})
+
+def image_edit(request, pk):
+    img = Trap_Image.objects.get(id=pk)
+    traps = Trap.objects.all()
+
+    if request.method == "POST":
+        form = ImageForm(request.POST, instance=img)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.save()
+            return redirect('images')
+    else:
+        form = ImageForm(instance=img)
+        print(img.id)
+        print(img.trap)
+
+    return render(request, 'classifier/image/edit.html', {'img': img, 'form': form, 'traps': traps})
 
 @login_required
 def traps(request):
     traps = Trap.objects.all()
-
     return render(request, 'classifier/trap/main.html', {'traps': traps})
 
 @login_required
@@ -81,8 +92,8 @@ def trap_new(request):
     if request.method == "POST":
         form = TrapForm(request.POST)
         if form.is_valid():
-            trap = form.save()
-            return HttpResponseRedirect(reverse('traps'))
+            form.save()
+            return redirect('traps')
     else:
         form = TrapForm()
 
@@ -90,12 +101,11 @@ def trap_new(request):
 
 @login_required
 def trap_delete(request, pk):
-    trap = get_object_or_404(Trap, pk=pk)
+    trap = Trap.objects.get(id=pk)
     trap.delete()
     return redirect('traps')
 
 @login_required
 def trap_detail(request, pk):
-    trap = get_object_or_404(Trap, pk=pk)
-
+    trap = Trap.objects.get(id=pk)
     return render(request, 'classifier/trap/detail.html', {'trap': trap})
