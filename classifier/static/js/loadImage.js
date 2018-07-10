a107bb25e51739eaef93b4988478b13a3f93114c
brunobelloni <bruno.belloni@hotmail.com>
2018-07-10 15:33:05 -0300

    commit

--- classifier/static/js/loadImage.js
+++ classifier/static/js/loadImage.js
@@ -2,11 +2,11 @@ function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
     reader.onload = function(e) {
-      $('#falseinput').attr('src', e.target.result);
       $('#id_image').val(e.target.result);
+      $('#id_src').attr('src', e.target.result);
     };
-    reader.readAsDataURL(input.files[0]);
 
+    reader.readAsDataURL(input.files[0]);
     document.getElementById('labelText').innerHTML = input.files[0].name;
   }
 }
