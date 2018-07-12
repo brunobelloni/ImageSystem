1c553995887a193694199da504b712e5ea798494
brunobelloni <bruno.belloni@hotmail.com>
2018-07-12 16:36:30 -0300

    commit

--- classifier/decoder_and_encoder.py
+++ classifier/decoder_and_encoder.py
@@ -1,34 +1,35 @@
-import io
 import cv2
 import base64
 import numpy as np
-from PIL import Image
-from io import BytesIO
 
-def stringToRGB(base64_string):
-    imgdata = base64.b64decode(str(base64_string))
-    print(imgdata)
-    image = Image.open(io.BytesIO(imgdata))
-    return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+def stringToImage(base64_string):
+    ''' Base64 to Numpy Array'''
+    if ', ' in base64_string:
+        _, base64_string = base64_string.split(', ')
+    elif ',' in base64_string:
+        _, base64_string = base64_string.split(',')
+    img = base64.b64decode(base64_string)
+    npimg = np.fromstring(img, dtype=np.uint8)
+    source = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    return source
 
-def cropImage(img, x, y, margin=None):
-    if margin == None:
-        margin = 25
+def cropImage(image, x, y, margin=None):
+    ''' Crop image '''
+    if(margin == None): margin = 25
+    y1, y2 = y - margin, y + margin
+    x1, x2 = x - margin, x + margin
+    return image[y1:y2, x1:x2]
 
-    y1, y2 = int(y) - int(margin), int(y) + int(margin)
-    x1, x2 = int(x) - int(margin), int(x) + int(margin)
-    return img[y1:y2, x1:x2]
+def imageTo64(img):
+    ''' Numpy Array to Base64 '''
+    _, data = cv2.imencode('.png', img)
+    bytes = data.tobytes()
+    b64 = "data:image/png;base64, " + base64.b64encode(bytes).decode("utf-8")
+    return b64
 
-def imgTo64(img):
-    pil_img = Image.fromarray(img)
-    buff = BytesIO()
-    pil_img.save(buff, format="JPEG")
-    new_image_string = base64.b64encode(buff.getvalue()).decode("utf-8")
-    return "data:image/png;base64, " + str(new_image_string)
-
-# imagem = cv2.imread("img.jpg")
-# imagem = cropImage(imagem, 25, 25)
-# cv2.imshow('teste', imagem)
-# cv2.waitKey()
-#
-# print(imgTo64(imagem))
+def getCropImg(b64, x, y, margin=None):
+    ''' Crop Base64 Images'''
+    img = stringToImage(b64)
+    img_crop = cropImage(img, x, y, margin)
+    base64_string = imageTo64(img_crop)
+    return base64_string
