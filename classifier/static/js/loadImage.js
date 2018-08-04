bc8c738642376835dc7e306c7f1585a0a2d8a690
brunobelloni <bruno.belloni@hotmail.com>
2018-08-03 22:42:02 -0300

    commit

--- classifier/static/js/loadImage.js
+++ classifier/static/js/loadImage.js
@@ -5,8 +5,6 @@ function readURL(input) {
       $('#id_image').val(e.target.result);
       $('#id_src').attr('src', e.target.result);
     };
-
     reader.readAsDataURL(input.files[0]);
-    document.getElementById('labelText').innerHTML = input.files[0].name;
   }
 }
