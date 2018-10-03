f2f370ac47e2ba63b1b4edf59cb9d1b09c75bb15
brunobelloni <bruno.belloni@hotmail.com>
2018-10-02 22:02:11 -0300

    bug fixed

--- backend/views.py
+++ backend/views.py
@@ -2,5 +2,5 @@ from django.contrib.auth.views import LoginView
 
 
 class MeuLoginView(LoginView):
-    tredirect_authenticated_user = True
+    redirect_authenticated_user = True
     template_name = 'login.html'
